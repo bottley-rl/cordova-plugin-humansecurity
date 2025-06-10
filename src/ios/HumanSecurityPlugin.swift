013d57d5ea40611e9face587616fdbab094e3837
Brad Ottley <bottley@rocketlawyer.co.uk>
2025-06-10 16:39:19 +0200

    use Set for domains array

--- src/ios/HumanSecurityPlugin.swift
+++ src/ios/HumanSecurityPlugin.swift
@@ -8,11 +8,13 @@ import Cordova
     @objc(start:)
     func start(command: CDVInvokedUrlCommand) {
         guard let appId = command.argument(at: 0) as? String,
-              let domains = command.argument(at: 1) as? [String] else {
+              let domainArray = command.argument(at: 1) as? [String] else {
             let result = CDVPluginResult(status: .error, messageAs: "Missing appId or domains")
             self.commandDelegate.send(result, callbackId: command.callbackId)
             return
         }
+
+        let domains = Set(domainArray)
 
         DispatchQueue.main.async {
             let policy = HSPolicy()
